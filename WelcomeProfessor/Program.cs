c560b50f55018a1f120a61d64ee822b6624134aa
iloozealot <iloozealot@gmail.com>
2023-11-13 11:52:04 -0600

    Add project files.

---
+++ WelcomeProfessor/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Console application?
+//Ask user for their name?
+//Welcome Tim and TIM as professor?
+//Welcome anyone else as student?
+
+string? firstName = null;
+
+Console.WriteLine("Welcome to the application: WelcomeProfessor");
+Console.WriteLine("This application was created by: Justin Spencer");
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine();
+
+Console.Write("What is your first name? ");
+firstName = Console.ReadLine();
+
+if (firstName == "Tim" || firstName == "TIM" || firstName == "tim")
+{
+    Console.WriteLine("Welcome, Professor.");
+}
+else
+{
+    Console.WriteLine("Welcome, student.");
+}
